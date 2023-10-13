aeab7f3913c0c13abd5de65ab1cff98da5ffb3ee
alefew <alefewyimer4@gmail.com>
2023-10-13 14:06:21 +0300

    maaping profile and service registration added

---
+++ Application/Profiles/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Application.Common.DTOs.Cinema;
+using Application.Common.DTOs.Movie;
+using Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Cinema, CinemaDTO>().ReverseMap();
+        CreateMap<Cinema, CreateCinemaDTO>().ReverseMap();
+        CreateMap<Cinema, UpdateCinemaDTO>().ReverseMap();
+
+        CreateMap<Movie, MovieDTO>().ReverseMap();        
+        CreateMap<Movie, CreateMovieDTO>().ReverseMap();
+        CreateMap<Movie, UpdateMovieDTO>().ReverseMap();
+    }
+}
+
